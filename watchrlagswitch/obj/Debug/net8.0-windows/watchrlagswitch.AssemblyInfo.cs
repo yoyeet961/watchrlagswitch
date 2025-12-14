4a56905efd2cf318c11f1e7f14507efe7c75e11a
yoyeet961 <oxana.rw@gmail.com>
2025-12-14 22:03:49 +0200

    readme fix

--- watchrlagswitch/obj/Debug/net8.0-windows/watchrlagswitch.AssemblyInfo.cs
+++ watchrlagswitch/obj/Debug/net8.0-windows/watchrlagswitch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("watchrlagswitch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87985fd5ac4ecfd341420a00011b4dfa1d255fa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("watchrlagswitch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("watchrlagswitch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
